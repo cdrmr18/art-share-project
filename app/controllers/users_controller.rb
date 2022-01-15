bd99026c6c6bf4842c297526942a6ca532cc007a
Kathy Tavia <cdrmr18@yahoo.com>
2022-01-15 20:26:08 +0100

    tested json params display for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def index
+        render plain: 'Hey there'
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
 end
